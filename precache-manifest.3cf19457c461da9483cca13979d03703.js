0b06a7fae6325e581a7f03cfa6ebae2144ec178b
gaurav halvey <halveygaurav@gmail.com>
2020-06-12 00:21:38 +0530

    Updates

---
+++ precache-manifest.3cf19457c461da9483cca13979d03703.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fff8abe99b6798d1ef30c9f80f8c0460",
+    "url": "/ABSWeb/index.html"
+  },
+  {
+    "revision": "2de74d9f84afa45644cc",
+    "url": "/ABSWeb/static/css/2.a63652b2.chunk.css"
+  },
+  {
+    "revision": "76dbeb46d066e17260ad",
+    "url": "/ABSWeb/static/css/main.6401a0a3.chunk.css"
+  },
+  {
+    "revision": "2de74d9f84afa45644cc",
+    "url": "/ABSWeb/static/js/2.294f572f.chunk.js"
+  },
+  {
+    "revision": "c3318f8322a7835f23974a92232fe5f6",
+    "url": "/ABSWeb/static/js/2.294f572f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "76dbeb46d066e17260ad",
+    "url": "/ABSWeb/static/js/main.3c01c5c0.chunk.js"
+  },
+  {
+    "revision": "fdec26e94d82799fae53",
+    "url": "/ABSWeb/static/js/runtime-main.2f0e7dfc.js"
+  },
+  {
+    "revision": "fc8e6cb11fe0da7f44a661f2b61fcbac",
+    "url": "/ABSWeb/static/media/searchIcon.fc8e6cb1.jpg"
+  },
+  {
+    "revision": "7e77dbfeb60fb77dbdcbafb38a9abeb1",
+    "url": "/ABSWeb/static/media/smallsignup.7e77dbfe.jpg"
+  }
+]);
